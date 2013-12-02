60ba336c40578c33e00f4c2c0912a4831f2a8629
Richard Tabor <Home@Richards-MacBook-Pro.local>
2013-12-02 13:36:19 -0500

    Cleaned up code a bit, removed variable errors

--- bean-theme-pricingtables.php
+++ bean-theme-pricingtables.php
@@ -1,14 +1,29 @@
 <?php
-/*--------------------------------------------------------------------*/
+/**
+ * The file controls the shortcode that is added to the Visual Editor.
+ *
+ *  
+ * @package Bean Plugins
+ * @subpackage PricingTables
+ * @author ThemeBeans
+ * @since Bean Framework 3.0
+ */
+ 
+ 
+ 
+ 
+/*===================================================================*/
 /*  TEXT WIDGET SHORTCODE FILTERS
-/*--------------------------------------------------------------------*/
+/*===================================================================*/
 add_filter('widget_text', 'shortcode_unautop', 10);
 add_filter('widget_text', 'do_shortcode', 10);
 
 
-/*--------------------------------------------------------------------*/
+
+
+/*===================================================================*/
 /*  PRICING TABLE SHORTCODE
-/*--------------------------------------------------------------------*/
+/*===================================================================*/
 function bean_pricing_table( $atts, $content = null ) {
 	global $shortcode_pricing_table;
 	extract(shortcode_atts(array(
@@ -17,19 +32,19 @@ function bean_pricing_table( $atts, $content = null ) {
 
 	switch ($columns) {
 		case '1':
-			$columnsClass .= 'one-column-table';
-			$widthClass .= 'five columns mobile-four centered';
+			$columnsClass = 'one-column-table';
+			$widthClass = 'five columns mobile-four centered';
 			break;
 		case '2':
-			$columnsClass .= 'two-column-table';
-			$widthClass .= 'six columns mobile-four';
+			$columnsClass = 'two-column-table';
+			$widthClass = 'six columns mobile-four';
 			break;
 		case '3':
-			$columnsClass .= 'three-column-table';
-			$widthClass .= 'four columns mobile-two';
+			$columnsClass = 'three-column-table';
+			$widthClass = 'four columns mobile-two';
 			break;
 		case '4':
-			$columnsClass .= 'four-column-table';
+			$columnsClass = 'four-column-table';
 			break;
 	}
 
@@ -43,7 +58,7 @@ function bean_pricing_table( $atts, $content = null ) {
 
 
 			$colClass = 'pricing-column'; $n = $i + 1;
-			$columnContent .= '<div class="'.$colClass.' '.$widthClass.' '.$highlightClass.'">';
+			$columnContent .= '<div class="'.$colClass.' '.$widthClass.'">';
 			$columnContent .= str_replace(array("\r\n", "\n", "\r"), array("", "", ""), $shortcode_pricing_table[$i]['content']); str_replace('', '', $shortcode_pricing_table[$i]['content']);
 			$columnContent .= '</div>';
 		}
@@ -58,15 +73,19 @@ function bean_pricing_table( $atts, $content = null ) {
 add_shortcode('pricing_table', 'bean_pricing_table');
 
 
-/*--------------------------------------------------------------------*/
+
+
+/*===================================================================*/
 /*  SINGLE COLUMN
-/*--------------------------------------------------------------------*/
+/*===================================================================*/
 function shortcode_pricing_column( $atts, $content = null ) {
 	global $shortcode_pricing_table;
 	extract(shortcode_atts(array(
     ), $atts));
 
-	$highlight = strtolower($highlight);
+	if ( isset( $highlight ) ) {
+		$highlight = strtolower($highlight);
+	}
 	$column['content'] = do_shortcode($content);
 	$shortcode_pricing_table[] = $column;
 
@@ -75,9 +94,11 @@ function shortcode_pricing_column( $atts, $content = null ) {
 add_shortcode('pricing_column', 'shortcode_pricing_column');
 
 
-/*--------------------------------------------------------------------*/
+
+
+/*===================================================================*/
 /*  PRICE INFO
-/*--------------------------------------------------------------------*/
+/*===================================================================*/
 function bean_shortcode_price_info( $atts, $content = null ) {
 	global $shortcode_pricing_table;
 	extract(shortcode_atts(array(
@@ -87,6 +108,8 @@ function bean_shortcode_price_info( $atts, $content = null ) {
 		'cost' => ''
     ), $atts));
 
+	$price_info = '';
+	
 	if ($highlighted) $price_info = '<div class="pricing-highlighted">'.$highlighted.'</div>';
 	if ($url) $price_info .= '<a href="'.$url.'">';
 		$price_info .= '<div class="table-mast">';
@@ -100,6 +123,4 @@ function bean_shortcode_price_info( $atts, $content = null ) {
 
 }
 add_shortcode('price_info', 'bean_shortcode_price_info');
-
-
 ?>
