72d45d7e8d7c9fc28e18cad529dc907ee8c31551
Kamil Waheed <kamilwaheed@hotmail.com>
2014-01-14 13:12:15 +0500

    Enqueue scripts/styles only when short code is being used

--- bean-pricingtables.php
+++ bean-pricingtables.php
@@ -6,8 +6,8 @@
  * Version: 1.1
  * Author: ThemeBeans
  * Author URI: http://themebeans.com/?ref=plugin_bean_pricing
- *  
- *   
+ *
+ *
  * @package Bean Plugins
  * @subpackage PricingTables
  * @author ThemeBeans
@@ -20,7 +20,7 @@
 /*===================================================================*/
 /*	MAKE SURE WE DO NOT EXPOSE ANY INFO IF CALLED DIRECTLY
 /*===================================================================*/
-if ( !function_exists( 'add_action' ) ) 
+if ( !function_exists( 'add_action' ) )
 {
 	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
 	exit;
@@ -64,98 +64,112 @@ $edd_updater = new EDD_SL_Plugin_Updater( BEANPRICINGTABLES_EDD_TB_URL, __FILE__
 /*===================================================================*/
 if ( ! class_exists( 'Bean_BeanPricingTables' ) ) :
 	class Bean_BeanPricingTables {
-	
+
 	    private $BEAN_TINYMCE_URI;
 	    private $BEAN_TINYMCE_DIR;
-	
-	
-	
-	
+
+
+
+
 		/*===================================================================*/
 		/*	CONSTRUCT
 		/*===================================================================*/
-	    function __construct() 
+	    function __construct()
 	    {
 	    	require_once( DIRNAME(__FILE__) . '/bean-theme-pricingtables.php' );
-	        
+
 	        $this->BEAN_TINYMCE_URI = plugin_dir_url(__FILE__) .'tinymce';
 	        $this->BEAN_TINYMCE_DIR = DIRNAME(__FILE__) .'/tinymce';
-	        
+
 	        add_action('init', array(&$this, 'action_admin_init'));
 	        add_action('admin_enqueue_scripts', array(&$this, 'action_admin_scripts_init'));
-	        add_action('wp_enqueue_scripts', array(&$this, 'action_frontend_scripts'));
+	        add_filter('the_posts', array(&$this, 'add_frontend_scripts_if_shortcode_being_used'));
 		}
-	
-	
-	
-	
+
+
+
+
 		/*===================================================================*/
 		/*	ENQUEUE FRONTEND
-		/*===================================================================*/ 
-		function action_frontend_scripts() 
+		/*===================================================================*/
+		function add_frontend_scripts_if_shortcode_being_used($posts)
 		{
-			$default_css_url = plugin_dir_url(__FILE__) . 'css/bean-pricingtables.css';
-			$default_js_url = plugin_dir_url(__FILE__) . 'js/bean-pricingtables.js';
-	
-			wp_enqueue_style( 'bean-pricingtables-style', $default_css_url, false, '1.0', 'all' );
-			wp_enqueue_script('bean-pricingtables', $default_js_url, 'jquery', '1.0', true);
+            if (empty($posts)) return $posts;
+
+            $shortcode_found = false;
+            foreach ($posts as $post) {
+                if (stripos($post->post_content, '[pricing_table') !== false) {
+                    $shortcode_found = true;
+                    break;
+                }
+            }
+
+            if ($shortcode_found) {
+                $default_css_url = plugin_dir_url(__FILE__) . 'css/bean-pricingtables.css';
+                $default_js_url = plugin_dir_url(__FILE__) . 'js/bean-pricingtables.js';
+
+                wp_enqueue_style( 'bean-pricingtables-style', $default_css_url, false, '1.0', 'all' );
+                wp_enqueue_script('bean-pricingtables', $default_js_url, 'jquery', '1.0', true);
+            }
+
+            return $posts;
 		}
-	
-	
-	
-	
+
+
+
+
 		/*===================================================================*/
 		/*	ENQUEUE ADMIN
-		/*===================================================================*/ 
-		function action_admin_scripts_init() 
+		/*===================================================================*/
+		function action_admin_scripts_init()
 		{
 			wp_enqueue_style( 'bean-pricingtable-popup', $this->BEAN_TINYMCE_URI . '/css/popup.css', false, '1.0', 'all' );
 			wp_localize_script( 'jquery', 'BeanPricingTables', array('plugin_folder' => plugin_dir_url(__FILE__)) );
 		}
-		
-		
-		
-		
+
+
+
+
 		/*===================================================================*/
 		/*	REGISTERS TINYMCE RICH EDITOR BUTTONS
-		/*===================================================================*/  
-		function action_admin_init() 
+		/*===================================================================*/
+		function action_admin_init()
 		{
 			if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') )
 				return;
-	
-			if ( get_user_option('rich_editing') == 'true' && is_admin() ) 
+
+			if ( get_user_option('rich_editing') == 'true' && is_admin() )
 			{
 				add_filter( 'mce_external_plugins', array(&$this, 'add_rich_plugins') );
 				add_filter( 'mce_buttons', array(&$this, 'register_rich_buttons') );
 			}
 		}
-	
-	
-	
-	
+
+
+
+
 		/*===================================================================*/
 		/*	DEFINE TINYMCE RICH EDITOR JS PLUGIN
-		/*===================================================================*/   
-		function add_rich_plugins( $plugin_array ) 
+		/*===================================================================*/
+		function add_rich_plugins( $plugin_array )
 		{
 			$plugin_array['BeanPricingTables'] = $this->BEAN_TINYMCE_URI . '/plugin.js';
 			return $plugin_array;
 		}
-	
-	
-	
-	
+
+
+
+
 		/*===================================================================*/
 		/*	ADDS TINYMCE BUTTON
-		/*===================================================================*/   
-		function register_rich_buttons( $buttons ) 
+		/*===================================================================*/
+		function register_rich_buttons( $buttons )
 		{
 			array_push( $buttons, "|", 'bean_pricingtable_button' );
 			return $buttons;
 		}
 	}
-	
+
 	new Bean_BeanPricingTables;
 
 endif; //END class Bean_BeanPricingTables
